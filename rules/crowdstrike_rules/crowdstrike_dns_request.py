d563758660f96a2b4640359f02b0e83ec77e2811
jzandona <79932094+jzandona@users.noreply.github.com>
2023-05-02 19:57:30 -0700

    Update crowdstrike_dns_request.py

--- rules/crowdstrike_rules/crowdstrike_dns_request.py
+++ rules/crowdstrike_rules/crowdstrike_dns_request.py
@@ -4,7 +4,7 @@ from panther_base_helpers import (
 )
 
 # baddomain.com is present for testing purposes. Add domains you wish to be alerted on to this list
-DENYLIST = ["baddomain.com","slickdeals.net7"]
+DENYLIST = ["baddomain.com","slickdeals.net8"]
 
 
 def rule(event):
